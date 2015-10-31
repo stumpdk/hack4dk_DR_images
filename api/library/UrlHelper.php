e015d591615b31b2a892d719108c917681325ff6
StumpDK <stumpdenmark@hotmail.com>
2015-10-31 23:25:21 +0000

    corrected url di issue

--- api/library/UrlHelper.php
+++ api/library/UrlHelper.php
@@ -8,7 +8,7 @@ class UrlHelper
             $subFolder = '/' . $di->get('serverLocation')[0];
         }
         
-        $protocol = strpos($_SERVER["SERVER_PROTOCOL"], 'HTTPS') === false ? 'https://' : 'http://';//strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
+        $protocol = strpos($_SERVER["SERVER_PROTOCOL"], 'HTTPS') === false ?  'http://' : 'https://';//strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
         return $protocol . str_replace(':80','', $_SERVER['HTTP_HOST']) . $subFolder;
     }
 }
