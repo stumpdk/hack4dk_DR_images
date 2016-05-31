1211d68386f47026ab031a1cd7404c59919ddf2b
StumpDK <stumpdenmark@hotmail.com>
2016-05-31 08:23:55 +0000

    Added comments in Users model

--- application/models/Users.php
+++ application/models/Users.php
@@ -4,11 +4,17 @@ use Phalcon\Mvc\Model;
 
 class Users extends Model
 {
+    /**
+     *  Check if the user is logged in
+     */ 
     public function checkLogin(){
         $this->getDI()->get('session')->start();
         return $this->getDI()->get('session')->has('fb_access_token');
     }
     
+    /**
+     *  The logput method resets the session information of the user
+     */ 
     public function logout()
     {
         $this->getDI()->get('session')->remove('fb_access_token');
@@ -17,11 +23,17 @@ class Users extends Model
         $this->getDI()->get('session')->remove('fb_user_first_name');
     }
     
+    /**
+     *  Get the Facebook name of the user
+     */ 
     public function getFbName()
     {
         return $this->getDI()->get('session')->get("fb_user_first_name");
     }
     
+    /**
+     * Get the Facebook user id of the user
+     */ 
     public function getFbId(){
         $session = $this->getDI()->get('session');
         
